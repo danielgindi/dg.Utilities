83377a337ab1de0c476bb5b68cea940e10a30fd6
Daniel Cohen Gindi <danielgindi@gmail.com>
2013-06-10 16:19:51 +0300

    Long-Long key

---
+++ Serialization/SerializableLongLongKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace dg.Utilities.Serialization
+{
+    [Serializable()]
+    public class SerializableLongLongKey : SerializableDoubleKey<Int64, Int64>
+    {
+        public SerializableLongLongKey()
+        {
+        }
+        public SerializableLongLongKey(Int64 A, Int64 B)
+            : base(A, B)
+        {
+        }
+    }
+}
