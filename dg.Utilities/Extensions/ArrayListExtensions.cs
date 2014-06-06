3b1d0bc8ff089e61cff7978335f130664d94cba9
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-06-06 10:29:30 +0300

    Find method for ArrayList

---
+++ dg.Utilities/Extensions/ArrayListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace dg.Utilities
+{
+    public static class ArrayListExtensions
+    {
+        public static object Find(this ArrayList input, Predicate<object> match)
+        {
+            foreach (object t in input)
+            {
+                if (match(t)) return t;
+            }
+            return null;
+        }
+    }
+}
