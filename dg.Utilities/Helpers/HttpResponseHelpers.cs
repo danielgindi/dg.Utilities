1f45a7452e7db9b97953cd8ef56f967d471a8501
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-03-08 00:32:30 +0200

    Added HttpResponse helpers

---
+++ dg.Utilities/Helpers/HttpResponseHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.IO.Compression;
+
+namespace dg.Utilities
+{
+    public static class HttpResponseHelpers
+    {
+        public static void EnableCompressionInResponseToRequest(HttpResponse response, HttpRequest request)
+        {
+            if ((request.Headers["Accept-Encoding"] ?? "").ToLower().Contains("gzip"))
+            {
+                response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+                response.AppendHeader("Content-Encoding", "gzip");
+            }
+        }
+    }
+}
