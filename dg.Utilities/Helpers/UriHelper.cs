d31b446abac4688df606f253f7b31d3f55bbb43b
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-03-27 19:23:13 +0300

    Added UriHelper for escaping uris

---
+++ dg.Utilities/Helpers/UriHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.IO.Compression;
+
+namespace dg.Utilities
+{
+    public static class UriHelper
+    {
+        /// <summary>
+        /// Escapes a uri safely.
+        /// If the uri is null - a null will be returned.
+        /// If the uri is invalid - it will be returned as it is.
+        /// If the uri is valid - it will be correctly escaped using <code>Uri.EscapeUriString(...)</code>
+        /// 
+        /// In any case - no exceptions will be thrown
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public static string EscapeUri(string uri)
+        {
+            if (uri == null) return null;
+            if (uri.Length == 0) return "";
+
+            try
+            {
+                return Uri.EscapeUriString(uri);
+            }
+            catch
+            {
+                return uri;
+            }
+        }
+    }
+}
