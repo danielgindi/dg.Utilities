394fe50af1eec2db2647f6e59ce387bf002a26cb
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-04-07 20:16:09 +0300

    Added base64 helpers

---
+++ dg.Utilities/Helpers/Base64Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.IO.Compression;
+
+namespace dg.Utilities
+{
+    public static class Base64Helper
+    {
+        public static string ToString(string base64, bool ignoreErrors = false)
+        {
+            try
+            {
+                return Encoding.UTF8.GetString(Convert.FromBase64String(base64));
+            }
+            catch
+            {
+                if (ignoreErrors)
+                {
+                    return "";
+                }
+
+                throw;
+            }
+        }
+
+        public static string Encode(string data, Base64FormattingOptions options = Base64FormattingOptions.None)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(data), options);
+        }
+    }
+}
