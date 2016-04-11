2ecf9565769a730dc9130d3dd329e23fd8953fd7
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-04-11 17:22:30 +0300

    Added a JsonHelper

---
+++ dg.Utilities/Helpers/JsonHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace dg.Utilities.Helpers
+{
+    public static class JsonHelper
+    {
+        public static void WriteValue(object value, StringBuilder sb)
+        {
+            if (value == null)
+            {
+                sb.Append(@"null");
+            }
+            else if (value is string)
+            {
+                sb.Append(((string)value).ToJavaScript('"', true));
+            }
+            else if (value is bool)
+            {
+                sb.Append((bool)value == true ? @"true" : @"false");
+            }
+            else if (value is IDictionary)
+            { // Must come before IEnumerable
+                var dic = (IDictionary)value;
+                bool first = true;
+                sb.Append('{');
+
+                foreach (object key in dic.Keys)
+                {
+                    if (!first) sb.Append(',');
+                    else first = false;
+
+                    sb.Append(StringHelper.EncodeJavaScript(key.ToString(), '"', true));
+                    sb.Append(':');
+                    WriteValue(dic[key], sb);
+                }
+                sb.Append('}');
+            }
+            else if (value is IEnumerable)
+            {
+                bool first = true;
+                sb.Append('[');
+
+                foreach (object arg in (IEnumerable)value)
+                {
+                    if (!first) sb.Append(',');
+                    else first = false;
+
+                    WriteValue(arg, sb);
+                }
+                sb.Append(']');
+            }
+            else
+            {
+                // Invariant culture, for printing numbers with "." as decimals
+                sb.AppendFormat(CultureInfo.InvariantCulture, @"{0}", value);
+            }
+        }
+
+        public static string Encode(object value)
+        {
+            StringBuilder sb = new StringBuilder();
+            WriteValue(value, sb);
+            return sb.ToString();
+        }
+    }
+}
