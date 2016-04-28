383eb14146515ee3d4112a001e9b02fe738843cd
Daniel Cohen Gindi <danielgindi@gmail.com>
2016-04-28 14:07:45 +0300

    .Helpers not needed in namespace

--- dg.Utilities/GoogleCloudMessaging/NotificationPayload.cs
+++ dg.Utilities/GoogleCloudMessaging/NotificationPayload.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
-using dg.Utilities.Helpers;
 
 namespace dg.Utilities.GoogleCloudMessaging
 {
--- dg.Utilities/Helpers/JsonHelper.cs
+++ dg.Utilities/Helpers/JsonHelper.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 
-namespace dg.Utilities.Helpers
+namespace dg.Utilities
 {
     public static class JsonHelper
     {
